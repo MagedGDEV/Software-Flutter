0e59333dca064a6756d5b2fd0cb1e6152a695540
Maged Alosali <magedalosali211@g,ail.com>
2022-02-10 19:00:15 +0200

    Done the boss challenge in the course and add all the required features

--- magic-8/lib/main.dart
+++ magic-8/lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      Magic8(),
+    );
+
+class Magic8 extends StatefulWidget {
+  @override
+  _Magic8State createState() => _Magic8State();
+}
+
+class _Magic8State extends State<Magic8> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    ballNumber = Random().nextInt(5) + 1;
+                  });
+                },
+                child: Image.asset('images/ball$ballNumber.png'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
