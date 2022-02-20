80109df9d373b76cfd9c89d3ebe091915f5ed834
Maged Alosali <magedalosali211@g,ail.com>
2022-02-20 23:37:03 +0200

    Add all the buttons and achieved the same UI of the required application

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -5,19 +5,92 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+  void playSound(int audio) {
+    player.play('note$audio.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('ClickMe'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(1);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.orange,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(2);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(3);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(4);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.teal,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(5);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(6);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.purple,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(7);
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
