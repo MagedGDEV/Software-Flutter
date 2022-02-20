edac9549a7ae15f8d4367e6467341c072ca656f1
Maged Alosali <magedalosali211@g,ail.com>
2022-02-20 04:47:55 +0200

    Play a sound from local assets file

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+//import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -9,7 +10,15 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+              child: Text('ClickMe'),
+            ),
+          ),
         ),
       ),
     );
